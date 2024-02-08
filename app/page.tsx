a71aa41c2e327471de862f0018c511b3d57102e2
Jeff Soriano <soriano.jeffm@gmail.com>
2024-02-08 10:55:01 -0800

    Fix mobile spacing issue

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from '@/components/LoginForm'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center overflow-x-hidden	p-8">
       <LoginForm />
     </main>
   )
